7db14746cc8ce6328997cfdc44fbf9634b054347
Yuhang Lin <ylin19@uvm.edu>
2018-03-13 12:19:56 -0400

    Add basic code for the project

---
+++ src/sorry/Card.java
@@ -0,0 +1,17 @@
+/**
+ * Store the information about the card.
+ */
+package sorry;
+
+/**
+ * @author Yuhang Lin
+ *
+ */
+public class Card {
+
+	public String info;
+	
+	public Card(String info) {
+		this.info = info;
+	}
+}
---
+++ src/sorry/Main.java
@@ -0,0 +1,20 @@
+/**
+ * Main class for the game
+ */
+package sorry;
+
+/**
+ * @author Yuhang Lin
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
