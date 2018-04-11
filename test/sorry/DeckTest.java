9cd69660fcecd7aaeabbc7851e8cb311daad7d41
Yuhang Lin <ylin19@uvm.edu>
2018-04-11 08:32:54 -0400

    Add test cases for Deck

---
+++ test/sorry/DeckTest.java
@@ -0,0 +1,14 @@
+package sorry;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DeckTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
