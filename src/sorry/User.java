a0ec3d61737c85890721b9094e9f588051261dee
Christopher Bratkovics <christopher.bratkovics@uvm.edu>
2018-04-12 19:14:04 -0400

    user and computer classes

---
+++ src/sorry/User.java
@@ -0,0 +1,25 @@
+package sorry;
+
+public class User extends Player{
+	
+	private PieceColor color;
+	private int movement;
+	private boolean occupied;
+	
+	public User(PieceColor c) {
+		super(c);
+	}
+	 
+	public PieceColor getColor(){
+		return color;
+	}
+	 
+	public void Move(int squareNum){
+		movement = squareNum;
+	}
+		
+	public void canMove(boolean isOccupied){
+		occupied = isOccupied;
+	}
+
+}
