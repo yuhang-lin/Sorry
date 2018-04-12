9d5b6bb7ef6ec62d44140b15247c5afc0e4e8428
Yuhang Lin <ylin19@uvm.edu>
2018-04-11 22:18:46 -0400

    Test the number of consecutive cards that are the same

--- test/sorry/DeckTest.java
+++ test/sorry/DeckTest.java
@@ -2,13 +2,28 @@ package sorry;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 
 class DeckTest {
 
-	@Test
+	/**
+	 * The number of two consecutive cards that are the same should be no more than 25 out of 200.
+	 */
+	@RepeatedTest(10)
+	@DisplayName("Repeat testing two consecutive cards that are the same")
 	void test() {
-		fail("Not yet implemented");
+		Deck deck = new Deck();
+		int counter = 0;
+		Card card = new Card("testing","testing");
+		for (int i = 0; i < 200; i++) {
+			Card temp = deck.draw();
+			if (temp.getName().equals(card.getName())) {
+				counter++;
+			}
+			card = temp;
+		}
+		assertTrue(counter <= 25, "Number of consecutive that are the same is: " + counter);
 	}
 
 }
