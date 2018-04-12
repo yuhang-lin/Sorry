d8d897ac41ca2731f6fc282241eea25c9258c204
Yuhang Lin <ylin19@uvm.edu>
2018-04-12 16:36:13 -0400

    Increase number of tests && Use new constructor

--- test/sorry/DeckTest.java
+++ test/sorry/DeckTest.java
@@ -10,12 +10,12 @@ class DeckTest {
 	/**
 	 * The number of two consecutive cards that are the same should be no more than 25 out of 200.
 	 */
-	@RepeatedTest(10)
+	@RepeatedTest(20)
 	@DisplayName("Repeat testing two consecutive cards that are the same")
 	void test() {
 		Deck deck = new Deck();
 		int counter = 0;
-		Card card = new Card("testing","testing");
+		Card card = new Card("testing");
 		for (int i = 0; i < 200; i++) {
 			Card temp = deck.draw();
 			if (temp.getName().equals(card.getName())) {
