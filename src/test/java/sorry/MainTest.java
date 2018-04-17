504a91a67b1af885ab62eba121a422176ecb4aff
Yuhang Lin <ylin19@uvm.edu>
2018-04-17 12:02:51 -0400

    Add display name

--- src/test/java/sorry/MainTest.java
+++ src/test/java/sorry/MainTest.java
@@ -2,11 +2,13 @@ package sorry;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class MainTest {
 
 	@Test
+	@DisplayName("Compare NumUsed before and after restoring")
 	void test() {
 		Main main = new Main();
 		int prevNumUsed = main.deck.getNumUsed();
