9ce0dd307ab54f8c1375f658958677d0b3dbc17c
Yuhang Lin <ylin19@uvm.edu>
2018-04-24 18:07:37 -0400

    Move Choice to a public class

--- src/main/java/sorry/Computer.java
+++ src/main/java/sorry/Computer.java
@@ -18,29 +18,6 @@ public class Computer extends Player {
 	private SmartLevel smartLevel;
 	private ArrayList<Choice> choiceList = new ArrayList<>();
 	private HashMap<Integer, Piece> boardMap = new HashMap<>();
-	
-	class Choice implements Comparable<Choice>{
-		Piece piece;
-		ArrayList<Integer> move;
-		int score;
-		
-		public Choice(Piece piece, ArrayList<Integer> move, int score) {
-			this.piece = piece;
-			this.move = move;
-			this.score = score;
-		}
-
-		@Override
-		public int compareTo(Choice o) {
-			if (this.score == o.score) {
-				return 0;
-			} else if (this.score > o.score) {
-				return 1;
-			}
-			return -1;
-		}
-		
-	}
 
 	public Computer(PieceColor c, NiceLevel n, SmartLevel s) {
 		super(c);
